eeea1711517c069d53819311538745685b48f1dd
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2023-05-12 05:16:26 -0700

    Update copy on <CalibrateGoto>

--- src/components/CalibrateGoto.tsx
+++ src/components/CalibrateGoto.tsx
@@ -5,6 +5,7 @@ import { ConnectionContext } from "@/stores/ConnectionContext";
 
 export default function CalibrateGoto() {
   const connectionCtx = useContext(ConnectionContext);
+  let showStatus = false;
 
   const [status, setStatus] = useState<any[]>([]);
   const [connecting, setConnecting] = useState(false);
@@ -30,18 +31,14 @@ export default function CalibrateGoto() {
     socket.addEventListener("error", (err) => {
       setConnecting(false);
       console.log("Error", err);
-      setStatus((prev) => prev.concat(JSON.stringify(err)));
     });
   }
 
   return (
     <>
-      <h1>Calibrate Goto</h1>
-      <h2>Instructions</h2>
+      <h2>Calibrate Goto</h2>
+      <p></p>
       <ol>
-        <li className="mb-2">
-          Focus the Dwarf II using the mobile app from Dwarf Labs.
-        </li>
         <li className="mb-2">
           Point the telescope lens towards an area of the sky where stars,
           planets, or the Moon are visible.
@@ -49,48 +46,87 @@ export default function CalibrateGoto() {
         <li className="mb-2">
           Click Calibrate to begin the calibration process.
         </li>
+        <li className="mb-2">
+          You must view the progress of the calibration on the Dwarf II mobile
+          app.
+        </li>
       </ol>
-      <button
-        className="btn btn-secondary"
-        disabled
-        onClick={gotoCalibratation}
-      >
+      <p>
+        API Note: The API does not currently return info about whether the
+        calibration is ok or failed.
+      </p>
+      <button className="btn btn-primary" onClick={gotoCalibratation}>
         Calibrate
       </button>
 
-      <span className="ms-3">Coming soon...</span>
       {connecting && <span>Connecting...</span>}
-      {status.length > 0 && <pre>{JSON.stringify(status, null, 2)}</pre>}
+      {showStatus &&
+        status.map((s, i) => <pre key={i}>{JSON.stringify(s, null, 2)}</pre>)}
     </>
   );
 }
 
 /*
-[
-  {
-    "motorId": 2,
-    "interface": 10110,
-    "direction": 1,
-    "distance": 1602,
-    "code": 0
-  },
-  {
-    "motorId": 2,
-    "interface": 10100,
-    "limit": 0,
-    "code": -22
-  },
-  {
-    "motorId": 2,
-    "interface": 10111,
-    "direction": 1,
-    "position": 1600,
-    "code": 0
-  },
-  {
-    "interface": 11003,
-    "value": 100,
-    "code": 0
-  }
-]
+// TODO: not enough info to tell the progress of the calibration
+
+{
+  "interface": 11003,
+  "value": 98,
+  "code": 0
+}
+{
+  "motorId": 2,
+  "interface": 10110,
+  "direction": 1,
+  "distance": 17756,
+  "code": 0
+}
+{
+  "motorId": 2,
+  "interface": 10100,
+  "limit": 0,
+  "code": -22
+}
+{
+  "motorId": 2,
+  "interface": 10111,
+  "direction": 1,
+  "position": 1600,
+  "code": 0
+}
+{
+  "motorId": 1,
+  "interface": 10110,
+  "direction": 0,
+  "distance": 32759,
+  "code": 0
+}
+{
+  "motorId": 1,
+  "interface": 10100,
+  "limit": 0,
+  "code": -22
+}
+{
+  "motorId": 1,
+  "interface": 10111,
+  "direction": 0,
+  "position": 1600,
+  "code": 0
+}
+{
+  "motorId": 1,
+  "interface": 10100,
+  "code": 0
+}
+{
+  "motorId": 2,
+  "interface": 10100,
+  "code": 0
+}
+{
+  "motorId": 1,
+  "interface": 10100,
+  "code": 0
+}
 */
