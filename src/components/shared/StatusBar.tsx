cbb7385e8baac71059a6f0eab1f68ae09d5277f0
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2023-05-13 06:25:39 -0700

    show '--' in status bar if no values

--- src/components/shared/StatusBar.tsx
+++ src/components/shared/StatusBar.tsx
@@ -8,21 +8,30 @@ export default function StatusBar() {
     ? "Connected"
     : "Not Connected";
   let coords =
-    `Lat: ${connectionCtx.latitude?.toFixed(5)}, ` +
-    `Lon: ${connectionCtx.longitude?.toFixed(5)}`;
-  let gain = `Gain: ${connectionCtx.gain}`;
-  let exp = `Exp: ${connectionCtx.exposure} sec`;
-  let ir = `IR: ${connectionCtx.ir === 0 ? "Cut" : "Pass"}`;
-  let binning = `IR: ${connectionCtx.binning === 0 ? "1x1" : "2x2"}`;
+    `Lat: ${connectionCtx.latitude?.toFixed(5) || "--"}, ` +
+    `Lon: ${connectionCtx.longitude?.toFixed(5) || "--"}`;
+  let gain = connectionCtx.gain == undefined ? "--" : connectionCtx.gain;
+  let exp =
+    connectionCtx.exposure === undefined
+      ? "--"
+      : `${connectionCtx.exposure} sec`;
+  let ir =
+    connectionCtx.ir === undefined
+      ? "--"
+      : `${connectionCtx.ir === 0 ? "Cut" : "Pass"}`;
+  let binning =
+    connectionCtx.binning === undefined
+      ? "--"
+      : `${connectionCtx.binning === 0 ? "1x1" : "2x2"}`;
 
   return (
     <div className=" mb-2 mt-2">
       <span>{connection}</span>
       <span className="ms-4"> {coords}</span>
-      <span className="ms-4">{gain}</span>
-      <span className="ms-4">{exp}</span>
-      <span className="ms-4">{ir}</span>
-      <span className="ms-4">{binning}</span>
+      <span className="ms-4">Gain: {gain}</span>
+      <span className="ms-4">Exp: {exp}</span>
+      <span className="ms-4">IR: {ir}</span>
+      <span className="ms-4">Binning: {binning}</span>
     </div>
   );
 }
